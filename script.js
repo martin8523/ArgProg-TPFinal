120046790a43f4d58cad3747d776b04280d8b2ce
martin8523 <110629999+martin8523@users.noreply.github.com>
2023-04-16 12:21:33 -0300

    A verr

--- script.js
+++ script.js
@@ -109,7 +109,8 @@ $.ajax({ method: "GET", url: "http://jsonplaceholder.typicode.com/users" })
             templateUL.appendChild(elem);
         });
         HTMLResponse.appendChild(templateUL);
-    })
+    }
+)
 
     function almacenavar(){
       var campoNombre = camponombre.value;
