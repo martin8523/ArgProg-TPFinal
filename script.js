31ef6116661576c46894894a0a50f0f9f1bb0602
martin8523 <martin.f.ramon@gmail.com>
2023-04-17 02:05:52 -0300

    arrreglos
    modificado clientes
    modficada funcion array form 2
    madificados algunos estilos

--- script.js
+++ script.js
@@ -84,38 +84,48 @@ $(document).ready(function(){
 
 
 
-        const HTMLResponse = document.querySelector("#app");
+const HTMLResponse = document.querySelector("#app");
 const templateUL = document.createElement(`ul`);
 
-$.ajax({ method: "GET", url: "http://jsonplaceholder.typicode.com/users" })
+$.ajax({ method: "GET", url: "http://jsonplaceholder.typicode.com/posts" })
 
     //users: es la RESPUESTA DE LA PETICION
-    .done(function (users) {
+    .done(function (posts) {
 
         //Mostrar por consola la respuesta de la API
-        console.log(users)
+        console.log(posts)
 
-        //Mostrar respuesta de API en un componente HTML
-       // document.getElementById("app").innerHTML = users;
-
-        //Mostrar respuesta de API en un componente HTML recorriendo cada usuario
-        users.forEach((user) => {
-
-
-            let elem = document.createElement("div");
+        for (let index = 0; index < 6; index++) {
+         
+          
+            let elem = document.createElement("button");
             elem.appendChild(
-                document.createTextNode(`Nombre: ${user.name}, Ciudad: ${user.address.city}`)
+                document.createTextNode(`Titulo: ${posts[index].title}, Comentario: ${posts[index].body}`)
             );
             templateUL.appendChild(elem);
-        });
-        HTMLResponse.appendChild(templateUL);
-    }
-    )
+            HTMLResponse.appendChild(templateUL);
+        }
+      })  
+        
 
-    function almacenavar(){
-      var campoNombre = camponombre.value;
-      var campoApellido = campoapellido.value;
-      let campoEmail
-
-      console.log(campoNombre, campoApellido);
+  
+var camposHoja1 =[]
+function almacenavar(){
+      for (let index = 0; index < 5; index++) {
+        camposHoja1[index] = document.getElementById(index).value
+      }
     }
+
+    const HTMLResponse2 = document.querySelector("#form2Hoja1");
+    const templateUL2 = document.createElement(`ul`);
+    function completar(){
+            let elem = document.createElement("p");
+            elem.appendChild(
+                document.createTextNode("Nombre: " + camposHoja1[0]+"  Apellido: " +camposHoja1[1]+" Mail: " 
+                + camposHoja1[2]+" Fecha: " + camposHoja1[3]+"  Comentarios: " +camposHoja1[4])
+                
+            );
+            templateUL2.appendChild(elem);       
+            HTMLResponse2.appendChild(templateUL2);
+    }
+  
